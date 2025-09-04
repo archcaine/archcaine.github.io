a19b4fd820d0041039183c96082bf36d6c3577ee
Ava <createchivity@gmail.com>
2025-09-04 10:51:23 -0400

    Update caineos.sh

--- resources/caineos.sh
+++ resources/caineos.sh
@@ -3,13 +3,13 @@ sudo wget https://colaciane.github.io/archcaine/archcaine/archcaine-setup.s
 archcaine-setup.sh
 echo Thank you for using ARCH-CAINE :)
 sudo pacman -Syu
-echo You will need to do Y/N for packages, don't say yes to all, a lot are optional applications you may not want!
-echo Essential ArchCaine packages are going to be installed regardless, and you won't need to answer yes or no :)
+echo "You will need to do Y/N for packages, don't say yes to all, a lot are optional applications you may not want!"
+echo "You will be told if something is an essential package."
 sudo pacman -S nano 
-echo Plasma is a popular desktop interface, highly customizable and is highly recommended to use!
-echo Plasma, while recommended, is not essential, so you will be able to do y/n
+echo "Plasma is a popular desktop interface, highly customizable and is highly recommended to use!"
+echo "Plasma, while recommended, is not essential."
 sudo pacman -S plasma
-echo 'apt' package is not recommended for Arch Linux unless you know how to PROPERLY set it up.
+echo "'apt' package is not recommended for Arch Linux unless you know how to PROPERLY set it up."
 sudo pacman -S apt
 sudo pacman -S kitty 
 sudo pacman -S power-profiles-daemon 
