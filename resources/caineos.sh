48ae10ebd8fb953f02cee2bf62ab71025da27540
Ava <createchivity@gmail.com>
2025-09-04 11:09:07 -0400

    Update caineos.sh

--- resources/caineos.sh
+++ resources/caineos.sh
@@ -1,23 +1,24 @@
-sudo pacman -S wget
-sudo wget https://colaciane.github.io/archcaine/archcaine/archcaine-setup.s
-archcaine-setup.sh
-echo Thank you for using ARCH-CAINE :)
-sudo pacman -Syu
+sudo pacman -S --noconfirm wget
+sudo wget https://colaciane.github.io/archcaine/archcaine/archcaine-setup.sh
+sudo ./archcaine-setup.sh
+echo "Thank you for using ARCH-CAINE :)"
+sudo pacman -Syu --noconfirm
 echo "You will need to do Y/N for packages, don't say yes to all, a lot are optional applications you may not want!"
-echo "You will be told if something is an essential package."
+echo "Essential applications will be installed without prompt, so don't worry."
 sudo pacman -S nano 
 echo "Plasma is a popular desktop interface, highly customizable and is highly recommended to use!"
 echo "Plasma, while recommended, is not essential."
 sudo pacman -S plasma
 echo "'apt' package is not recommended for Arch Linux unless you know how to PROPERLY set it up."
 sudo pacman -S apt
+echo "Kitty Terminal is recommended"
 sudo pacman -S kitty 
-sudo pacman -S power-profiles-daemon 
+sudo pacman -S --noconfirm power-profiles-daemon
 # sudo wget https://colacaine.github.io/archcaine/archcaine/misc.zip
-sudo pacman -S zip 
-sudo pacman -S unzip 
+sudo pacman -S --noconfirm zip 
+sudo pacman -S --noconfirm unzip 
 sudo pacman -S nautilus
-echo npm is recommended
+echo npm is recommended, not required.
 sudo pacman -S npm
 sudo pacman -R archaine-setup.sh 
 # if didn't already
